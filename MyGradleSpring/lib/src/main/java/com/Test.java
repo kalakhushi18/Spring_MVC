0cb4d00c67392f03e611be452365e7e9106d50da
kalakhushi18 <khushikala18@gmail.com>
2022-02-25 15:44:25 +0530

    gradle commit

---
+++ MyGradleSpring/lib/src/main/java/com/Test.java
@@ -0,0 +1,19 @@
+package com;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		Student student = (Student)context.getBean("student");
+		
+		student.setId(101);
+		System.out.println(student.getId());
+		
+
+	}
+	
+}
