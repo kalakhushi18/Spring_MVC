e3b61461e4ed8bd5c818a5ebc3e0612a70c05548
kalakhushi18 <khushikala18@gmail.com>
2022-03-03 10:40:41 +0530

    jdbc

--- maven_spring_jdbc/src/main/java/com/capg/maven_spring_jdbc/App.java
+++ maven_spring_jdbc/src/main/java/com/capg/maven_spring_jdbc/App.java
@@ -15,8 +15,6 @@ public class App
        Studentdao st = context.getBean(Studentdao.class);
        Details s = new Details();
        s.setStid(1000);
-       
-       
        s.setStname("khushi");
        int x = st.saveStudent(s);
 		
